3f1a776f17e05a2acb93ba9d3544daed21b35e3d
EnningerGabriel <gabrielenninger@gmail.com>
2020-01-05 23:18:40 -0300

    Second Class - Start to implement Board and Piece

--- src/program/Program.java
+++ src/program/Program.java
@@ -1,13 +1,20 @@
 package program;
 
-import boardGame.Position;
+import boardGame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
+
+		Board board = new Board(8, 8);
 		
-Position pos = new Position(3, 5);
-System.out.println(pos);
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
