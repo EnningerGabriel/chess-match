3a03738f593c500dc3849459e866e7a3dff21e6d
EnningerGabriel <gabrielenninger@gmail.com>
2020-01-08 23:56:50 -0200

    third day - some moves, impossible moves and improving the UI

--- src/boardGame/Piece.java
+++ src/boardGame/Piece.java
@@ -14,7 +14,7 @@ public abstract class Piece {
 	}
 	
 	public abstract boolean[][] possibleMoves();
-	public boolean posibleMove(Position position) {
+	public boolean possibleMove(Position position) {
 		return possibleMoves()[position.getRow()][position.getColumn()];
 	}
 	public boolean isThereAnyPossibleMove() {
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardGame.Board;
 import boardGame.Piece;
+import boardGame.Position;
 
 public abstract class ChessPiece extends Piece {
 private Color color;
@@ -14,5 +15,13 @@ public ChessPiece(Board board, Color color) {
 public Color getColor() {
 	return color;
 }
+
+protected boolean isThereOpponentPiece(Position position) {
+	ChessPiece p = (ChessPiece)getBoard().piece(position);
+	return p != null && p.getColor() != color;
+	
+}
+
+
 
 }
