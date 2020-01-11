f96792ff402ffa40e9098e74494cca7237236bb0
EnningerGabriel <gabrielenninger@gmail.com>
2020-01-10 21:12:07 -0300

    UI improvements and more

--- src/program/UI.java
+++ src/program/UI.java
@@ -55,6 +55,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
 		System.out.println("Waiting " + chessMatch.getCurrentPlayer() + " player move");
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 	
 	
